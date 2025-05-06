806ad38758d5f21b19935f22241ea5f9dfcd5ebe
suyash-ranawat <101948257+suyash-ranawat@users.noreply.github.com>
2025-05-06 07:29:21 +0530

    Delete src/services/config.ts

--- src/services/config.ts
+++
@@ -1,9 +0,0 @@
-export const API_BASE_URL = 'https://api.search.com';
-
-export const API_KEYS = {
-  key: 'Iyo-KzAgODkoPCQmNSEp',
-  auth: 'Ig,,',
-  uid: 'VGVxdm9jZjVKdVpkT2g3Z25ONVBkZz09',
-  sub: '',
-  reset: '111'
-};
